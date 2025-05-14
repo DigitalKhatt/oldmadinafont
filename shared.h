201fe147c7af3cbea48f8069358f925acd93725c
Amine Anane <ananeamine@gmail.com>
2025-05-14 11:04:25 -0400

    Remove stdcall

--- shared.h
+++ shared.h
@@ -2,9 +2,9 @@
 
 #include "oldmadina.h"
 
-extern "C" __declspec(dllexport) Automedina* __stdcall font_create(OtLayout* layout, Font* font, bool extended);
+extern "C" __declspec(dllexport) Automedina* font_create(OtLayout* layout, Font* font, bool extended);
 
-extern "C" void __stdcall font_delete(OldMadina* p_obj) {
+extern "C" void font_delete(OldMadina* p_obj) {
 	try {
 		delete p_obj;
 	}
