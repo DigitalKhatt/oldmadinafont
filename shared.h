3c0d271f7e9b2740b94d5a9ca958548fe2ba6e2c
Amine Anane <ananeamine@gmail.com>
2026-07-18 14:14:09 -0400

    Use portable font plugin exports

--- shared.h
+++ shared.h
@@ -1,8 +1,9 @@
 #pragma once
 
+#include "fontplugin.h"
 #include "oldmadina.h"
 
-extern "C" __declspec(dllexport) Automedina* font_create(OtLayout* layout, MPFont* font, bool extended);
+extern "C" DIGITALKHATT_FONT_EXPORT Automedina* font_create(OtLayout* layout, MPFont* font, bool extended);
 
 extern "C" void font_delete(OldMadina* p_obj) {
 	try {
